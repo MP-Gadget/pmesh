7c9cb89c73d43409c0b1874ddee362e5fc97b408
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-04-18 18:48:25 -0700

    more documentation as I read them.

--- pypm/particlemesh.py
+++ pypm/particlemesh.py
@@ -15,25 +15,28 @@ class ParticleMesh(object):
     ParticleMesh provides an interface to solver for forces
     with particle mesh method
 
-    ParticleMesh object is a state machine. 
-    The typical routine is
-
-    1. Paint particles via :py:meth:`paint`
-    2. Real to Complex transform via :py:meth:`r2c`
-    3. Complex to Real transform via :py:meth:`c2r`, applying transfer functions
-    4. Read out force values     via :py:meth:`readout`
-    5. go back to 3, for other force components
+    ParticleMesh object is a state machine. Refer to :ref:`introduction` on 
+    the standard steps to use this object.
 
     Memory usage is twice the size of the FFT mesh. However, 
     Be aware the transfer functions may take more memory.
 
     Attributes
     ----------
+    np      : array_like (npx, npy)
+        The shape of the process mesh. This is the number of domains per direction.
+        The product of the items shall equal to the size of communicator. 
+        For example, for 64 rank job, np = (8, 8) is a good choice.
+        Since for now only 3d simulations are supported, np must be of length-2.
+        The default is try to split the total number of ranks equally. (eg, for
+        a 64 rank job, default is (8, 8)
+
     comm    : :py:class:`MPI.Comm`
-        the MPI communicator
+        the MPI communicator, (default is MPI.COMM_WORLD)
 
     Nmesh   : int
-        number of mesh points per side
+        number of mesh points per side. Only 3d simulations are supported by now,
+        and the true mesh is [Nmesh, Nmesh, Nmesh]
 
     BoxSize : float
         size of box
@@ -76,6 +79,8 @@ class ParticleMesh(object):
             pfft.Flags.PFFT_TRANSPOSED_OUT | pfft.Flags.PFFT_DESTROY_INPUT)
 
         self.real = pfft.LocalBuffer(self.partition).view_input()
+        self.real[:] = 0
+
         self.complex = pfft.LocalBuffer(self.partition).view_output()
 
         self.T = Timers(self.comm)
@@ -148,15 +153,33 @@ class ParticleMesh(object):
         with self.T['Decompose']:
             return self.domain.decompose(pos, smoothing=1.0,
                     transform=self.transform0)
+    def clear(self):
+        """ 
+        Clear the internal real canvas. 
+
+        This function simply set real[:] = 0. After :py:meth:`clear`, 
+        :py:meth:`paint` can correctly paint to the canvas.
+
+        Notes
+        -----
+        A freshly created :py:class:`ParticleMesh` object come with
+        a cleared canvas.
+    
+        """
 
     def paint(self, pos, mass=1.0):
         """ 
-        Paint particles into the internal real canvas.
+        Paint particles into the internal real canvas. 
 
         Transform the particle field given by pos and mass
         to the overdensity field in fourier space and save
-        it in the internal storage.
+        it in the internal storage. 
         A multi-linear CIC approximation scheme is used.
+
+        The function can be called multiple times: 
+        the result is cummulative. In a multi-step simulation where
+        :py:class:`ParticleMesh` object is reused,  before calling 
+        :py:meth:`paint`, make sure the canvas is cleared with :py:meth:`clear`.
 
         Parameters
         ----------
@@ -170,22 +193,25 @@ class ParticleMesh(object):
         Notes
         -----
         self.real is NOT the density field after this operation.
-        It has the dimension of density, but is divided by 
-        Nmesh **3; PFFT will adjust for this Nmesh**3 after r2c .
+        It has the dimension of mass but is divided by :code:`Nmesh**3`.
+        Density is :code:`real * Nmesh ** 3 / BoxSize ** 3`
 
+        :py:meth:`r2c` will convert to a density unit by dividing
+        by :code:`BoxSize**3`.
+
+        PFFT will adjust for this Nmesh**3 after r2c .
+    
         """
         with self.T['Paint']:
-            self.real[:] = 0
             cic.paint(pos, self.real, weights=mass, mode='ignore',
                     period=self.Nmesh, transform=self.transform)
-            self.real *= (1.0 / self.BoxSize) ** 3
 
     def r2c(self, pos=None, mass=1.0):
         """ 
         Perform real to complex FFT on the internal canvas.
 
         If pos and mass are given, :py:meth:`paint` is called to
-        paint the particles before running the fourier transform
+        paint the particles before running the fourier transform.
 
         Parameters
         ----------
@@ -197,8 +223,10 @@ class ParticleMesh(object):
 
         """
         if pos is not None:
+            self.clear()
             self.paint(pos, mass)
 
+        self.real *= (1.0 / self.BoxSize) ** 3
         if self.verbose:
             realsum = self.comm.allreduce(self.real.sum(dtype='f8'), MPI.SUM)
             if self.comm.rank == 0:

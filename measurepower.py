288a9a8faba71792d577eecf5e09e9dc78d2d0e5
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-04-19 17:23:28 -0700

    Properly calculate shotnoise and add documentation.

--- measurepower.py
+++ measurepower.py
@@ -10,11 +10,32 @@ from argparse import ArgumentParser
 import numpy
 from mpi4py import MPI
 
-parser = ArgumentParser()
+parser = ArgumentParser("Parallel Power Spectrum Calculator",
+        description=
+     """Calculating matter power spectrum from RunPB input files. 
+        Output is written to stdout, in Mpc/h units. 
+        PowerSpectrum is the true one, without (2 pi) ** 3 factor. (differ from Gadget/NGenIC internal)
 
-parser.add_argument("filename")
-parser.add_argument("Nmesh", type=int)
-parser.add_argument("BoxSize", type=float)
+     """,
+        epilog=
+     """
+        This script is written by Yu Feng, as part of `pypm' which depends on `pfft-python' and mpi4py. 
+        The author would like thank Marcel Schmittfull for the explanation on cic, shotnoise, and k==0 plane errors.
+     """
+        )
+
+parser.add_argument("filename", 
+        help='basename of the input, only runpb format is supported in this script')
+parser.add_argument("Nmesh", type=int, 
+        help='size of calculation mesh, recommend 2 * Ngrid')
+parser.add_argument("BoxSize", type=float, 
+        help='BoxSize in Mpc/h')
+parser.add_argument("--bunchsize", type=int, default=1024*1024*4,
+        help='Number of particles to read per rank. A larger number usually means faster IO, but less memory for the FFT mesh')
+parser.add_argument("--remove-cic", default='anisotropic', metavar="anisotropic|isotropic|none",
+        help='deconvolve cic, anisotropic is the proper way, see http://www.personal.psu.edu/duj13/dissertation/djeong_diss.pdf')
+parser.add_argument("--remove-shotnoise", action='store_true', default=False, 
+        help='removing the shot noise term')
 
 ns = parser.parse_args()
 logging.basicConfig(level=logging.DEBUG)
@@ -23,7 +44,7 @@ def main():
     pm = ParticleMesh(ns.BoxSize, ns.Nmesh)
 
     Ntot = 0
-    for round, P in enumerate(read_tpm(pm.comm, ns.filename)):
+    for round, P in enumerate(read_tpm(pm.comm, ns.filename, ns.bunchsize)):
         layout = pm.decompose(P['Position'])
         tpos = layout.exchange(P['Position'])
         #print tpos.shape
@@ -36,70 +57,45 @@ def main():
 
     pm.r2c()
     
-    def RemoveShotNoise(complex, w):
-        complex[:] -= 1. / Ntot
+    def RemoveShotnoise(complex, w):
+        complex[:] -= (1.0 / Ntot) ** 0.5
 
-    def CIC(complex, w):
+    def AnisotropicCIC(complex, w):
         for wi in w:
-            # convert to 
-            tmp = 1 - 2. / 3 * numpy.sin(0.5 * wi) ** 2
+            tmp = (1 - 2. / 3 * numpy.sin(0.5 * wi) ** 2) ** 0.5
             complex[:] /= tmp
 
     wout = numpy.empty(pm.Nmesh//2)
-    psout0 = numpy.empty(pm.Nmesh//2)
-    psout1 = numpy.empty(pm.Nmesh//2)
-    psout2 = numpy.empty(pm.Nmesh//2)
+    psout = numpy.empty(pm.Nmesh//2)
 
-    # measure the raw power spectrum, only shot noise is removed.
-    pm.push()
-    pm.transfer( [
+    chain = [
         TransferFunction.NormalizeDC,
         TransferFunction.RemoveDC,
-        RemoveShotNoise,
-        TransferFunction.PowerSpectrum(wout, psout0),
-    ])
-    wout *= pm.Nmesh / pm.BoxSize
-    psout0 *= (numpy.pi / pm.BoxSize) ** -3
-    pm.pop()
+    ]
 
-    # measure the power spectrum, correcting for CIC after shotnoise 
-    # removal
+    if ns.remove_cic == 'anisotropic':
+        chain.append(AnisotropicCIC)
+
+    if ns.remove_shotnoise:
+        chain.append(RemoveShotnoise)
+
+    chain.append(TransferFunction.PowerSpectrum(wout, psout))
+        
+    # measure the raw power spectrum, nothing is removed.
     pm.push()
-    pm.transfer( [
-        TransferFunction.NormalizeDC,
-        TransferFunction.RemoveDC,
-        CIC,
-        RemoveShotNoise,
-        TransferFunction.PowerSpectrum(wout, psout1),
-    ])
-    wout *= pm.Nmesh / pm.BoxSize
-    psout1 *= (numpy.pi / pm.BoxSize) ** -3
+    pm.transfer(chain)
+    kout = wout * pm.Nmesh / pm.BoxSize
+    psout *= (pm.BoxSize) ** 3
     pm.pop()
 
-    # measure the power spectrum, correcting for CIC after shotnoise 
-    # removal, but in the lazy way.
-    pm.push()
-    pm.transfer( [
-        TransferFunction.NormalizeDC,
-        TransferFunction.RemoveDC,
-        TransferFunction.PowerSpectrum(wout, psout2),
-    ])
-
-    tmp = 1.0 - 0.666666667 * numpy.sin(wout * 0.5) ** 2
-
-    wout *= pm.Nmesh / pm.BoxSize
-    psout2 *= (numpy.pi / pm.BoxSize) ** -3
-    psout2 /= tmp
-    psout2 -= 1.0 / Ntot
-    pm.pop()
+    if ns.remove_cic == 'isotropic':
+        tmp = 1.0 - 0.666666667 * numpy.sin(wout * 0.5) ** 2
 
     if pm.comm.rank == 0:
-        numpy.savetxt(stdout, zip(wout, psout0, psout1, psout2))
+        numpy.savetxt(stdout, zip(kout, psout), '%0.7g')
 
 import os.path    
-def read_tpm(comm, filename):
-    # read this many per chunk
-    BunchSize = 1024 * 1024
+def read_tpm(comm, filename, BunchSize):
 
     NpartPerFile = []
     if comm.rank == 0:

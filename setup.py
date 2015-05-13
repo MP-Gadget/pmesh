f911f8e817e07a43572d4c90f00351f96ee3391e
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-05-13 15:21:03 -0700

    remove mpi4py from compilation requirements.

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 import numpy
-import mpi4py
 import os
-compiler = mpi4py.get_config()['mpicc']
-os.environ['CC'] = compiler
-os.environ['LDSHARED'] = compiler + ' -shared'
 
 def myext(*args):
     return Extension(*args, include_dirs=["./", numpy.get_include()])
@@ -20,7 +16,7 @@ setup(
     author="Yu Feng",
     description="Particle Mesh in Python",
     package_dir = {'pypm': 'pypm'},
-    install_requires=['cython', 'numpy', 'mpi4py'],
+    install_requires=['cython', 'numpy'],
     packages= ['pypm'],
     requires=['numpy', 'mpi4py'],
     ext_modules = cythonize(extensions)

a57de6a2fbd5808237f8337531ad0a6474c4e74c
Yu Feng <rainwoodman@gmail.com>
2015-09-26 18:25:55 -0700

    Add test cases for k/w/x/r

--- tests/test-particlemesh.py
+++ tests/test-particlemesh.py
@@ -1,5 +1,6 @@
 from pypm.particlemesh import ParticleMesh
 from numpy.testing import assert_allclose
+
 def test_roundtrip_normalization():
     pm = ParticleMesh(10.0, 2)
     pm.real[:] = 1.0
@@ -9,3 +10,15 @@ def test_roundtrip_normalization():
     assert_allclose(pm.real, 1.0)
 
     
+def test_xkrw():
+    pm = ParticleMesh(10.0, 2)
+
+    for i in range(3):
+        assert(pm.r[i].shape[i] == 2)
+        assert(pm.x[i].shape[i] == 2)
+
+    for i in range(3):
+        # it happends for 2x2x2 the last dim shall also
+        # be 2 // 2 + 1 == 2
+        assert(pm.k[i].shape[i] == 2)
+        assert(pm.w[i].shape[i] == 2)

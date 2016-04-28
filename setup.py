16cbc552d7f0c00abdc9295de4bedba7c7a79257
Nick Hand <nhand@berkeley.edu>
2016-04-28 00:04:21 -0700

    bump the version one more time for TSC fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extensions = [
         ]
 
 setup(
-    name="pmesh", version="0.0.8",
+    name="pmesh", version="0.0.9",
     author="Yu Feng",
     description="Particle Mesh in Python",
     package_dir = {'pmesh': 'pmesh'},

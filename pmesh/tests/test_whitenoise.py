dc32d3c145df7d22b1ecfa53e25cf819f96382b5
Yu Feng <rainwoodman@gmail.com>
2016-12-27 18:06:48 -0800

    Add tests for 1d.

--- pmesh/tests/test_whitenoise.py
+++ pmesh/tests/test_whitenoise.py
@@ -35,3 +35,16 @@ def test_generate_2d():
 
     assert_array_equal(piece, truth)
 
+def test_generate_1d():
+    value = numpy.zeros((64), dtype='complex128')
+
+    generate(value, 0, value.shape, 1)
+
+    piece = numpy.zeros((32),dtype='complex128')
+    offset = [2]
+    offset = [2]
+    generate(piece, offset, value.shape, 1)
+    truth = value[
+        offset[0]:offset[0] + piece.shape[0]]
+
+    assert_array_equal(piece, truth)

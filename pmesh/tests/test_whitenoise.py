c49dd62b21382c89fa39ff4864b06676243c3fea
Yu Feng <rainwoodman@gmail.com>
2016-12-27 18:06:09 -0800

    allow 2d whitenoise field.

--- pmesh/tests/test_whitenoise.py
+++ pmesh/tests/test_whitenoise.py
@@ -20,3 +20,18 @@ def test_generate():
 
     assert_array_equal(piece, truth)
 
+def test_generate_2d():
+    value = numpy.zeros((64, 64), dtype='complex128')
+
+    generate(value, 0, value.shape, 1)
+
+    piece = numpy.zeros((32, 4),dtype='complex128')
+    offset = [2, 2]
+    offset = [2, 2]
+    generate(piece, offset, value.shape, 1)
+    truth = value[
+        offset[0]:offset[0] + piece.shape[0],
+        offset[1]:offset[1] + piece.shape[1]]
+
+    assert_array_equal(piece, truth)
+
--- pmesh/whitenoise.py
+++ pmesh/whitenoise.py
@@ -7,4 +7,17 @@ def generate(complex, start, Nmesh, seed):
     _start[:] = start
     _Nmesh[:] = Nmesh
 
-    _whitenoise.generate(complex, _start, _Nmesh, seed)
+    if complex.ndim == 3:
+        _whitenoise.generate(complex, _start, _Nmesh, seed)
+    elif complex.ndim <= 2:
+        # FIXME: this is not scale invariant though it is at least invariant
+        # against partition. Since 2d and 1d is only used for testing this
+        # may be good enough.
+        rng = numpy.random.RandomState(seed)
+        real = rng.normal(size=_Nmesh)
+        full = numpy.fft.irfftn(real)
+        full[...] *= numpy.prod(_Nmesh)
+        slices = tuple([slice(a, a + b) for a, b in zip(_start, complex.shape)])
+        complex[...] = full[slices]
+    else:
+        raise ValueError("Only knows how to make a whitenoise up to 3d")

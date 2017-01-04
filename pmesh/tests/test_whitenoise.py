616c25dc365af35a17aec1c380861c30224c60de
Yu Feng <rainwoodman@gmail.com>
2017-01-03 22:31:23 -0800

    assert the variance.

--- pmesh/tests/test_whitenoise.py
+++ pmesh/tests/test_whitenoise.py
@@ -4,15 +4,18 @@ from numpy.testing.decorators import skipif
 
 from pmesh.whitenoise import generate
 
-def test_generate():
-    value = numpy.zeros((64, 64, 64), dtype='complex128')
+def test_generate_3d():
+    Nmesh = 128
+    value = numpy.zeros((Nmesh, Nmesh, Nmesh//2 + 1), dtype='complex128')
 
-    generate(value, 0, value.shape, 1, unitary=False)
+    generate(value, 0, (Nmesh, Nmesh, Nmesh), 1, unitary=False)
+    assert_allclose(value.real.std(), 0.5 ** 0.5, rtol=1e-2)
+    assert_allclose(value.imag.std(), 0.5 ** 0.5, rtol=1e-2)
 
     piece = numpy.zeros((32, 4, 4),dtype='complex128')
     offset = [2, 2, 2]
     offset = [2, 2, 2]
-    generate(piece, offset, value.shape, 1, unitary=False)
+    generate(piece, offset, (Nmesh, Nmesh, Nmesh), 1, unitary=False)
     truth = value[
         offset[0]:offset[0] + piece.shape[0],
         offset[1]:offset[1] + piece.shape[1],
@@ -21,14 +24,16 @@ def test_generate():
     assert_array_equal(piece, truth)
 
 def test_generate_2d():
-    value = numpy.zeros((64, 64), dtype='complex128')
+    Nmesh = 1024
+    value = numpy.zeros((Nmesh, Nmesh//2 + 1), dtype='complex128')
 
-    generate(value, 0, value.shape, 1, unitary=False)
+    generate(value, 0, (Nmesh, Nmesh), 1, unitary=False)
+    assert_allclose(value.real.std(), 0.5 ** 0.5, rtol=1e-1)
+    assert_allclose(value.imag.std(), 0.5 ** 0.5, rtol=1e-1)
 
     piece = numpy.zeros((32, 4),dtype='complex128')
     offset = [2, 2]
-    offset = [2, 2]
-    generate(piece, offset, value.shape, 1, unitary=False)
+    generate(piece, offset, (Nmesh, Nmesh), 1, unitary=False)
     truth = value[
         offset[0]:offset[0] + piece.shape[0],
         offset[1]:offset[1] + piece.shape[1]]
@@ -36,14 +41,17 @@ def test_generate_2d():
     assert_array_equal(piece, truth)
 
 def test_generate_1d():
-    value = numpy.zeros((64), dtype='complex128')
+    Nmesh = 4096 * 32
+    value = numpy.zeros((Nmesh//2 + 1), dtype='complex128')
 
-    generate(value, 0, value.shape, 1, unitary=False)
+    generate(value, 0, (Nmesh,), 1, unitary=False)
+    assert_allclose(value.real.std(), 0.5 ** 0.5, rtol=1e-1)
+    assert_allclose(value.imag.std(), 0.5 ** 0.5, rtol=1e-1)
 
-    piece = numpy.zeros((32),dtype='complex128')
+    piece = numpy.zeros((8),dtype='complex128')
     offset = [2]
     offset = [2]
-    generate(piece, offset, value.shape, 1, unitary=False)
+    generate(piece, offset, (Nmesh,), 1, unitary=False)
     truth = value[
         offset[0]:offset[0] + piece.shape[0]]
 

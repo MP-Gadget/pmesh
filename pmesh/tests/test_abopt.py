4888458ea4eb62444997c6254285323654551b7e
Yu Feng <rainwoodman@gmail.com>
2017-06-18 12:24:33 -0700

    fix abopt related tests.

--- pmesh/tests/test_abopt.py
+++ pmesh/tests/test_abopt.py
@@ -1,20 +1,24 @@
 from __future__ import print_function
-from abopt.vmad2 import CodeSegment, Engine, statement, programme, ZERO, logger
 
 from numpy.testing import assert_raises, assert_array_equal, assert_allclose
 from numpy.testing.decorators import skipif
 import numpy
 import logging
 
-logger.setLevel(level=logging.WARNING)
+from pmesh.pm import ParticleMesh
+pm = ParticleMesh(BoxSize=1.0, Nmesh=(4, 4), dtype='f8', resampler='tsc')
 
 try:
-    from abopt.engines.pmesh import ParticleMeshEngine, ParticleMesh, RealField, ComplexField, check_grad
-    pm = ParticleMesh(BoxSize=1.0, Nmesh=(4, 4), dtype='f8', resampler='tsc')
-except ImportError:
-    pm = None
+    from abopt.vmad2 import CodeSegment, logger
+    has_abopt = True
+except:
+    has_abopt = False
 
-@skipif(pm == None)
+if has_abopt:
+    from pmesh.abopt import ParticleMeshEngine,  check_grad, Literal
+    logger.setLevel(level=logging.WARNING)
+
+@skipif(not has_abopt)
 def test_compute():
     def transfer(k): return 2.0
     engine = ParticleMeshEngine(pm)
@@ -29,7 +33,7 @@ def test_compute():
     norm = code.compute('sum', init={'r': field})
     assert_allclose(norm, field.cnorm() * 4)
 
-@skipif(pm == None)
+@skipif(not has_abopt)
 def test_gradient():
     def transfer(k):
         k2 = sum(ki **2 for ki in k)
@@ -41,18 +45,18 @@ def test_gradient():
     code.r2c(real='r', complex='c')
     code.transfer(complex='c', tf=transfer)
     code.c2r(complex='c', real='r')
-    code.norm(field='r', r='sum', metric=None)
+    code.multiply(x1='r', x2=Literal(0.1), y='r')
+    code.to_scalar(x='r', y='sum')
 
     field = pm.generate_whitenoise(seed=1234).c2r()
 
     norm = code.compute('sum', init={'r': field})
-    assert_allclose(norm, field.cnorm() * 4)
+    assert_allclose(norm, field.cnorm() * 4 * 0.1 ** 2)
     norm, _r = code.compute_with_gradient(('sum', '_r'), init={'r': field}, ginit={'_sum': 1.0})
-    assert_allclose(_r, field * 4 * 2)
+    assert_allclose(_r, field * 4 * 2 * 0.1 * 0.1)
 
-@skipif(pm == None)
 
-@skipif(pm == None)
+@skipif(not has_abopt)
 def test_paint():
     engine = ParticleMeshEngine(pm)
     code = CodeSegment(engine)
@@ -64,7 +68,7 @@ def test_paint():
 
     check_grad(code, 'density', 's', init={'s': s}, eps=1e-4, rtol=1e-2)
 
-@skipif(pm == None)
+@skipif(not has_abopt)
 def test_readout():
     engine = ParticleMeshEngine(pm)
     code = CodeSegment(engine)
